e25ea023732946ac7a057e22fb39818a417680a5
Zi-ji <zhuzhijie1003@outlook.com>
2022-06-04 14:23:19 +1000

    first commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import MobXIcon from '~icons/logos/mobx';
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<div />
+<p>
+	Visit <MobXIcon width={40} height={40} /> <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read
+	the documentation
+</p>
