a9f7670b1904980f6090897f8d3e8a5f940cc2b3
Zi-ji <zhuzhijie1003@outlook.com>
2022-06-06 01:12:51 +1000

    homeview buttons

--- src/lib/views/HomeView.svelte
+++ src/lib/views/HomeView.svelte
@@ -1,19 +1,23 @@
 <script lang="ts">
-	import ArrowIcon from '~icons/akar-icons/arrow-down';
+	import ProjectIcon from '~icons/ant-design/project-filled';
+	import PersonIcon from '~icons/bi/person-fill';
 </script>
 
 <h3>Hi, I am</h3>
 <h1>Richard Zhu</h1>
 <h2>I work with web and data.</h2>
+
 <p>
 	I am currently studying Master of IT at UNSW. I am also one of the CSESoc Projects Team. You know
 	what I&apos;m most proud of? I&apos;m 1/3 of logicpop.
 </p>
+
 <div class="button-container">
-	<button>My Projects <ArrowIcon width={20} height={20} /></button>
+	<button class="project-button">My Projects <ProjectIcon width={20} height={20} /></button>
+	<button class="about-button">About Me <PersonIcon width={20} height={20} /></button>
 </div>
 
-<style>
+<style lang="scss">
 	h1 {
 		font-size: 4rem;
 		line-height: 1;
@@ -33,25 +37,37 @@
 		font-weight: 500;
 	}
 	.button-container {
+		display: flex;
 		margin: 1rem 0 1rem 0;
 	}
 	.button-container button {
 		width: 10rem;
-		padding: 0.5rem 1rem 0.5rem 1rem;
+		padding: 0.5rem 0.75rem 0.5rem 0.75rem;
+		margin: 0 1rem 0 0;
 		display: flex;
 		align-items: center;
 		justify-content: space-between;
 		border-radius: 0.2rem;
-		border: 2px solid var(--primary-color);
-		background-color: var(--primary-color);
-		color: var(--white);
-		fill: var(--text-color);
 		cursor: pointer;
 		user-select: none;
 		transition-duration: var(--transition-time);
 	}
-	.button-container button:hover {
+	.project-button {
+		border: 2px solid var(--primary-color);
+		background-color: var(--primary-color);
+		color: var(--white);
+	}
+	.project-button:hover {
 		color: var(--primary-color);
+		background-color: var(--bg-color);
+	}
+	.about-button {
+		border: 2px solid var(--text-color);
+		background-color: var(--text-color);
+		color: var(--white);
+	}
+	.about-button:hover {
+		color: var(--text-color);
 		background-color: var(--bg-color);
 	}
 </style>
